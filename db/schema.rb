06ade8090c24681b79161ecf7999dc6454e199ac
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-12 22:32:08 +0900

    nameをvalidates設定しエラーが出たので1度コメントアウト

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_10_12_050541) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
