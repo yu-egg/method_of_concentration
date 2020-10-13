e815f73acd6eb1f177c44ad0d70420a6af90813c
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-10-13 17:28:02 +0900

    カウントダウン機能の実装、startボタンの動作確認まで

---
+++ app/views/users/javascript/concentrate.js
@@ -0,0 +1,28 @@
+(function(){
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var reset = document.getElementById('reset');
+  var start = document.getElementById('start');
+
+  var startTime;
+  var timeLeft;
+  var timeToCountDown  = 4 * 1000;
+
+    function countDown() {
+      setTimeout(function(){
+        // var elapsedTime = Date.now() - startTime;
+        // timeLeft = timeToCountDown - elapsedTime;
+        timeLeft = timeToCountDown - (Date.now() - startTime);
+        console.log(timeLeft);
+        countDown();
+      }, 10);
+    }
+
+    start.addEventListener('click', function(){
+      startTime = Date.now();
+      countDown();
+    });
+})();
